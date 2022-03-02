4bf3536326479aa685049897b7374303325f73ed
bercivarga <65171545+bercivarga@users.noreply.github.com>
2022-03-02 22:16:15 +0100

    add fancy delete todo animation

--- src/mvc/controller.js
+++ src/mvc/controller.js
@@ -1,20 +1,41 @@
-import { model } from './model'
-import View from '../mvc/views/View'
-import TodoList from './views/TodoList'
-import AddTodo from './views/AddTodo'
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
+
+import { model } from "./model";
+import View from "../mvc/views/View";
+import TodoList from "./views/TodoList";
+import AddTodo from "./views/AddTodo";
 
 export function init() {
-  View.render(model.user.name)
-  TodoList.render(model.todos, handleDeleteTodo)
-  AddTodo.render()
+  View.render(model.user.name);
+  TodoList.render(model.todos, handleDeleteTodo);
+  AddTodo.render();
+  AddTodo.addButtonEventListener(handleAddTodo);
 }
 
+function handleAddTodo() {}
+
 function handleDeleteTodo(id) {
-  TodoList.clear()
-  model.todos = model.todos.filter(todo => todo.id !== id)
-  refresh()
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      model.todos = model.todos.filter((todo) => todo.id !== id);
+      TodoList.clear();
+      refresh();
+    } else if (result.isDenied) {
+      return;
+    }
+  });
 }
 
 function refresh() {
-  TodoList.render(model.todos, handleDeleteTodo)
+  TodoList.render(model.todos, handleDeleteTodo);
 }
--- src/mvc/views/AddTodo.js
+++ src/mvc/views/AddTodo.js
@@ -1,11 +1,18 @@
 import { ViewClass } from './View'
 
 class AddTodo extends ViewClass {
+  button = undefined
+
+  addButtonEventListener(callback) {
+    this.button.addEventListener('click', callback)
+  }
+
   render() {
     const button = document.createElement('button')
     button.innerText = 'Add todo'
     button.className = 'add-button'
 
+    this.button = button
     this.parentEl.after(button)
   }
 }
